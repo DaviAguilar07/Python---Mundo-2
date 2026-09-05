407c4788819a9b8590120e7cc133e1ecb3046a6f
Davi Aguilar <aguilardavi44@gmail.com>
2026-09-05 16:10:26 -0300

    Atividade 43 - Calculando o IMC

---
+++ ex043.py
@@ -0,0 +1,18 @@
+peso = float(input('Qual o seu peso? '))
+altura = float(input('Qual a sua altura? '))
+
+imc = peso / (altura*altura)
+
+print('O seu IMC é {:.2f}'.format(imc))
+
+if imc < 18.5:
+    print('Abaixo do peso.')
+elif 18.5 <= imc <= 25:
+    print('Peso ideal.')
+elif 25 < imc <= 30:
+    print('Sobrepeso.')
+elif 30 < imc <= 40:
+    print('Obesidade.')
+else:
+    print('Obesidade mórbida')
+    
