4b705c28d568f60d4bd261c2d69709a2d3e1db13
Davi Aguilar <aguilardavi44@gmail.com>
2026-09-10 19:44:29 -0300

    Atividade 55

---
+++ ex055.py
@@ -0,0 +1,18 @@
+maior = 0
+menor = 0
+
+for pessoas in range(1, 6):
+    peso = float(input("Qual o peso da {}° pessoa? ".format(pessoas)))
+
+    if pessoas == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+
+        if peso < menor:
+            menor = peso
+
+print("A pessoa mais pesada tem {}Kg".format(maior))
+print("A pessoa mais leve tem {}Kg".format(menor))
